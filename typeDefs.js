81c93485913d0e228000ee1d8210af2126c060d0
Matthew Pagan <mastapegs01@gmail.com>
2020-11-07 17:36:10 -0500

    refactor

--- typeDefs.js
+++ typeDefs.js
@@ -1,12 +1,12 @@
 const { gql } = require('apollo-server')
 
 const typeDefs = gql`
+  type Query {
+    me: Person
+  }
   type Person {
     name: String
     role: String
-  }
-  type Query {
-    me: Person
   }
 `
 
